411082f3fd667810d51c4e6f2badc0d9bc2be6e3
boboborghi <bobo.borghi@me.com>
2015-05-08 14:20:24 -0700

    Added tab bar view controller.

---
+++ CoffeeLoyaltyApp/MainTabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarViewController.swift
+//  CoffeeLoyaltyApp
+//
+//  Created by Andrea Borghi on 5/8/15.
+//  Copyright (c) 2015 E. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
