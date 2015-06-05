5a0d397af40027e3dc246e1a03b53fcd0caee206
Masataka Mori <masataka.mori20@gmail.com>
2015-06-05 15:26:37 -0700

    Create Drinks Class and Tea Class

---
+++ CoffeeLoyaltyApp/Drink.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  CoffeeLoyaltyApp
+//
+//  Created by Masataka Mori on 6/5/15.
+//  Copyright (c) 2015 E. All rights reserved.
+//
+
+import Foundation
+
+enum Size {
+    case Single
+    case Double
+}
+enum Temperature {
+    case Hot
+    case Cold
+}
+
+class Drinks {
+
+
+    var name:String = ""
+    var image:String = ""
+    var price:Double = 0.0
+    var size:Size
+    var temperature: Temperature
+    
+    init(name:String){
+
+    }
+
+}
---
+++ CoffeeLoyaltyApp/Tea.swift
@@ -0,0 +1,18 @@
+//
+//  Tea.swift
+//  CoffeeLoyaltyApp
+//
+//  Created by Masataka Mori on 6/5/15.
+//  Copyright (c) 2015 E. All rights reserved.
+//
+
+import Foundation
+
+class Tea: Drinks {
+    var fruit:String = ""
+    var teaTypes:String = ""
+
+    init(name:String, size:Size, temperature:Temperature, fruits:String, teaTypes:String){
+
+    }
+}
