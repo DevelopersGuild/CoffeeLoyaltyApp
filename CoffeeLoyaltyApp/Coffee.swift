9bc67a0a972b1bb76a2f2d8c4b68c299a9a48f68
Robin Goh <robingoh@Robins-MacBook-Air.local>
2015-06-12 15:21:23 -0700

    Added new Coffee subclass of Drink

---
+++ CoffeeLoyaltyApp/Coffee.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee.swift
+//  CoffeeLoyaltyApp
+//
+//  Created by Robin Goh on 6/12/15.
+//  Copyright (c) 2015 E. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Drink {
+   var coffeeTypes:String = ""
+
+   init(name:String, size:Size, temperature:Temperature, coffeeTypes:String){
+
+   }
+}
